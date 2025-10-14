6e5486e38c3a46502bb6c06ba03dbe754f9357e7
Francisco Mauro de Lima <frmauro8@gmail.com>
2025-10-14 18:01:56 -0300

    create categories module

--- src/app/categorias/categoria/categoria.ts
+++ src/app/categorias/categoria/categoria.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.scss'
 })
 export class Categoria {
+  camposForm!: FormGroup;
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      name: new FormControl('', Validators.required)
+    });
+   }
+
+   salvar(): void {
+    //this.camposForm.markAllAsTouched();
+
+    if (this.camposForm.valid) {
+      console.log('Categoria salva com sucesso!', this.camposForm.value);
+    } else {
+      console.log('Formulário inválido. Por favor, preencha todos os campos obrigatórios.');
+    }
+   }
+
+   isCampoInvalido(campo: string): boolean {
+    const controle = this.camposForm.get(campo);
+    return controle ? controle.invalid && (controle.dirty || controle.touched) : false;
+   }
 
 }
