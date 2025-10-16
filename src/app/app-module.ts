3806aede3a7d25bcaa96c337a740a4556d2d00df
Francisco Mauro de Lima <frmauro8@gmail.com>
2025-10-16 14:59:45 -0300

    create landpage and autentication

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { Landingpage } from './landingpage/landingpage';
+import { AuthInterceptor } from './auth.interceptor';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Landingpage
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   bootstrap: [App]
 })
